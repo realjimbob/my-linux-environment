3745bc6f8e356f047df5519639a535270052f31f
James Spinks <jspinks@gmail.com>
2017-11-07 16:33:40 +0000

    Create install.sh
    Added installation script to copy the other scripts into ~/bin etc.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#set -x
+
+# Create local bin directory if it doesn't exist
+mkdir -p ~/bin
+
+# Install the various scripts here into the bin directory
+# Don't forget to set the permissions for execution
+# No backups and just overwrites anything that might be there already
+cp reboot-required.sh ~/bin/reboot-required
+chmod +x ~/bin/reboot-required
+
+# Update the PATH?
+
