9e38735b0c84a059974ec7d741f04dc4a4fad372
James Spinks <jspinks@gmail.com>
2017-11-14 14:24:30 +0000

    Added steps to deploy the start-ssh-agent.sh script

--- install.sh
+++ install.sh
@@ -8,8 +8,11 @@ mkdir -p ~/bin
 # Install the various scripts here into the bin directory
 # Don't forget to set the permissions for execution
 # No backups and just overwrites anything that might be there already
+
 cp reboot-required.sh ~/bin/reboot-required
-chmod +x ~/bin/reboot-required
+cp start-ssh-agent.sh ~/bin/start-ssh-agent
+
+chmod +x ~/bin/reboot-required ~/bin/start-ssh-agent
 
 # Update the PATH?
 
